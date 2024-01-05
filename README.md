2c7efce9c9728497831495d682125f2a38914a27
Joseph Markus <joseph.markus@simplybusiness.co.uk>
2024-01-05 07:08:51 +0000

    Add overflow navigation

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is an overflow navigation which shows as many navigation items as it can fit on the screen. Anything that doesn't fit will be rendered in a hide/reveal container under 'More &darr;' button.
 
-## Getting Started
+This [Next.js](https://nextjs.org/) app has been bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-First, run the development server:
+## Run
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+1. Install dependencies with `yarn install`
+1. Run `yarn dev`
+1. Open [http://localhost:3000](http://localhost:3000)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,179 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useCallback, useEffect, useRef, useState } from "react";
+import debounce from "lodash.debounce";
+import styles from "./page.module.css";
+
+type NavProps = {
+  label: string;
+  show: boolean;
+  width: "auto" | number;
+}[];
+
+const PRIMARY_NAV: NavProps = [
+  { label: "Home", show: true, width: "auto" },
+  { label: "Israel-Gaza war", show: true, width: "auto" },
+  { label: "Cost of Living", show: true, width: "auto" },
+  { label: "War in Ukraine", show: true, width: "auto" },
+  { label: "Climate", show: true, width: "auto" },
+  { label: "UK", show: true, width: "auto" },
+  { label: "World", show: true, width: "auto" },
+  { label: "Business", show: true, width: "auto" },
+  { label: "Politics", show: true, width: "auto" },
+  { label: "Culture", show: true, width: "auto" },
+  { label: "Tech", show: true, width: "auto" },
+  { label: "Science", show: true, width: "auto" },
+  { label: "Health", show: true, width: "auto" },
+  { label: "Family & Education", show: true, width: "auto" },
+];
+
+const SECONDARY_NAV: NavProps = [
+  { label: "In Pictures", show: true, width: "auto" },
+  { label: "Newsbeat", show: true, width: "auto" },
+  { label: "BBC Verify", show: true, width: "auto" },
+  { label: "Disability", show: true, width: "auto" },
+];
+
+const DEBOUNCE_IN_MS = 500;
 
 export default function Home() {
+  const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
+  const toggleRef = useRef<HTMLButtonElement>(null);
+  const [isCalculated, setIsCalculated] = useState(false);
+  const [showOverflow, setShowOverflow] = useState(false);
+  const [links, setLinks] = useState(PRIMARY_NAV);
+
+  const handleResize = useCallback(() => {
+    const toggleRefWidth =
+      toggleRef.current?.getBoundingClientRect().width || 0;
+    const leftAndRightPadding = 16 * 2;
+    const availableWidth = Math.floor(
+      window.outerWidth - leftAndRightPadding - toggleRefWidth,
+    );
+    let itemTotalWidth = 0;
+    const newLinks = links.map((link) => {
+      const width = link.width as number;
+      itemTotalWidth += width;
+
+      return {
+        ...link,
+        show: width > 0 && itemTotalWidth < availableWidth ? true : false,
+      };
+    });
+
+    setLinks(newLinks);
+  }, [links]);
+
+  const handleClick = () => {
+    setShowOverflow(!showOverflow);
+  };
+
+  // Check how many items fit on screen on initial render AFTER nav
+  // items rendered on page and their widths are available via refs
+  useEffect(() => {
+    handleResize();
+    // no need for handleResize as dependency as it ends up in an infinite loop
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isCalculated]);
+
+  // Calculate navigation item width once on render and save the value
+  useEffect(() => {
+    const withWidth = links.map((link, index) => {
+      const itemWidth = itemsRef.current[index]?.getBoundingClientRect().width;
+      return {
+        ...link,
+        width: Math.floor(itemWidth || 0),
+      };
+    });
+
+    setLinks(withWidth);
+    setIsCalculated(true);
+    // no need for links as dependency as this should only be called once
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Attach event listeners for browser window resizing
+  // Debounce calls, so function isn't invoked on every pixel changed
+  useEffect(() => {
+    window.addEventListener("resize", debounce(handleResize, DEBOUNCE_IN_MS), {
+      passive: true,
+    });
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [handleResize]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div className={styles.outerContainer}>
+        <nav className={styles.container}>
+          <ul className={styles.list}>
+            {links.map((link, index) => (
+              <li
+                key={link.label}
+                className={styles.listItem}
+                aria-hidden={!link.show}
+                ref={(el) => (itemsRef.current[index] = el)}
+              >
+                <a href="#" className={`${styles.link} ${styles.linkPrimary}`}>
+                  <span
+                    className={`${styles.linkText} ${styles.linkTextWithBorder}`}
+                  >
+                    {link.label}
+                  </span>
+                </a>
+              </li>
+            ))}
+            <li className={styles.listItem}>
+              <span className={styles.buttonContainer}>
+                <button
+                  ref={toggleRef}
+                  type="button"
+                  className={`${styles.button} ${
+                    showOverflow && styles.buttonActive
+                  }`}
+                  aria-expanded={showOverflow}
+                  aria-haspopup={true}
+                  onClick={handleClick}
+                >
+                  More &darr;
+                </button>
+              </span>
+            </li>
+          </ul>
+        </nav>
+        {showOverflow && (
+          <div className={styles.overflowContainer}>
+            <div className={styles.container}>
+              <ul className={`${styles.list} ${styles.listWrap}`}>
+                {links
+                  .filter((link) => !link.show)
+                  .map((link) => (
+                    <li key={link.label}>
+                      <a
+                        href="#"
+                        className={`${styles.link} ${styles.linkSecondary}`}
+                      >
+                        <span className={styles.linkText}>{link.label}</span>
+                      </a>
+                    </li>
+                  ))}
+                {SECONDARY_NAV.map((link) => (
+                  <li key={link.label}>
+                    <a
+                      href="#"
+                      className={`${styles.link} ${styles.linkSecondary}`}
+                    >
+                      <span className={styles.linkText}>{link.label}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </main>
-  )
+  );
 }
